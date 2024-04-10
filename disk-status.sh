17f803075e223fa3da940d24e7cc2a26aac5e1c5
Royal-Reddy-Yaparla <iamroyalreddy@gmail.com>
2024-04-10 20:17:59 +0530

    disk-user-alert-mail

--- disk-status.sh
+++ disk-status.sh
@@ -8,18 +8,7 @@
 # Input: disc_limit and to mail id
 ################################################
 
-ID=$(id -u)
 
-# Colors
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOG_FILE="/tmp/$0-$TIMESTAMP.log"
-
-# USAGE=$(df -h | grep -Ev 'tmpf' | grep -Ev 'efivarfs' | awk '{print $5F}' | cut -d "%" -f 1)
 DISK_USAGE=$(df -hT | grep -vE 'tmp|efivarfs' | tail -n +2)
 DISK_THRUSHOLD=$1
 TO_MAIL=$2
@@ -35,4 +24,4 @@ do
     fi
 done <<< $DISK_USAGE
 
-sh mail.sh "DevOps Team" "High Disk Usage" "$MESSEAGE" "$TO_MAIL" "ALERT Hign Disc Usage"
+sh mail.sh "DevOps Team" "High Disk Usage" "$MESSEAGE" "$TO_MAIL" "ALERT Hign Disc Usage" "$DISK_THRUSHOLD"
